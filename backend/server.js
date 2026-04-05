c820a4e7b00270a417870cd64ac2e97d7dbc1903
Kumari Nikki <nikkibellla66@gmail.com>
2026-04-06 01:36:35 +0530

    add serviceAppointment model

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import {clerkMiddleware} from '@clerk/express'
 import {connectDB } from './config/db.js'
 import doctorRouter from "./routes/doctorRoutes.js";
 import serviceRouter from "./routes/serviceRouter.js";
+import appointmentRouter from "./routes/appointmentRouter.js"
 const app=express()
 const port=4000;
 // middlewares
@@ -17,10 +18,12 @@ app.use(clerkMiddleware());
 //DB
 connectDB()
 //Routes
-
 app.use("/api/doctors", doctorRouter)
 
 app.use("/api/services", serviceRouter)
+
+app.use("/api/appointments", appointmentRouter)
+
 app.get('/',(req,res)=>{
     res.send("API Working")
 })
