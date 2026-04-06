95f497f2b21120f1e20844fddbf54be76c42b435
Kumari Nikki <nikkibellla66@gmail.com>
2026-04-06 19:05:42 +0530

    fix: correct allowedHeaders typo and clean allowedOrigins in CORS setup

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,26 @@ const app=express()
 const port=4000;
 
 const allowedOrigins=[
-    
+    "http://localhost:5173",
+    "http://localhost:5174",
 ]
 
 // middlewares
+app.use(cors(
+    {
+        origin:function(origin, callback){
+            if(!origin) return callback(null,true);
+            if(allowedOrigins.includes(origin))
+            {
+                return callback(null,true)
+            }
+            return callback(new Error("Not allowed by CORS"))
+        },
+        credentials:true,
+        methods:["GET","POST", "DELETE","OPTIONS"],
+        allowedHeaders:["Content-Type","Authorization"]
+    }
+))
 app.use(cors())
 app.use(express.json({limit:"20mb"}));
 app.use(express.urlencoded({limit:"20mb", extended:true}));
