0ed3e714c14fc2e1ce6714996d5a99ace2d184c8
Kumari Nikki <nikkibellla66@gmail.com>
2026-04-10 15:21:23 +0530

    Add Navbar and Hero component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Hero from './pages/Hero';
 
-function App() {
+const App = () => {
   return (
-    <div>
-       <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Hero />} />
+    </Routes>
   )
 }
 
