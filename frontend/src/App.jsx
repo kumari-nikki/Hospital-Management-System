fed74ce69bf2252946952dc3b8c0f7a18e72df4f
Kumari Nikki <nikkibellla66@gmail.com>
2026-04-12 11:05:02 +0530

    Add the ServiceDetails component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Doctors from './pages/Doctors'
 import DoctorDetail from './pages/DoctorDetails'
 import Service from './pages/Service'
+import ServiceDetailPage from './pages/ServiceDetailPage'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
          <Route path="/doctors" element={<Doctors/>}></Route>
            <Route path="/doctors/:id" element={<DoctorDetail/>}></Route>
              <Route path="/services" element={<Service/>}></Route>
+              <Route path="/services/:id" element={<ServiceDetailPage/>}></Route>
       </Routes>
     </div>
   )
